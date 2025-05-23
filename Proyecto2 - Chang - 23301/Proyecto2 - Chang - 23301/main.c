0447e6e95f2daf21610f3b3272b9f3b0bd606304
juanchang23301 <cha23301@uvg.edu.gt>
2025-05-22 18:38:23 -0600

    Create main.c

---
+++ Proyecto2 - Chang - 23301/Proyecto2 - Chang - 23301/main.c
@@ -0,0 +1,28 @@
+/************************************************************************
+* Universidad del Valle de Guatemala
+* IE2023: Programación de Microcontroladores
+* Conexión UART
+*
+* Autor: Juan René Chang Lam
+*
+* Descripción: Control de una garra robótica mediante potenciómetros, USART y EEPROM
+*
+* Conexiones:
+*   - Servo Base: PD5 (OC0B)
+*   - Servo Brazo1: PD6 (OC0A)
+*   - Servo Brazo2: PB1 (OC1A)
+*   - Servo Pinza: PB2 (OC1B)
+*   - Potenciometro Base: PC0 (ADC0)
+*   - Potenciometro Brazo1: PC1 (ADC1)
+*   - Potenciometro Brazo2: PC2 (ADC2)
+*   - Potenciometro Pinza: PC3 (ADC3)
+*   - USART: PD0 (RX), PD1 (TX)
+*   - PushButton Modo: PB0 
+*   - LED Modo Manual: PD2
+*   - LED Modo USART: PD3
+*   - LED Modo EEPROM: PD4
+*   - Botón Reproducir Secuencia: PD7 
+*   - Botón Guardar Posición: PB3 
+*   - LED Indicador Posición: PC4
+*   - LED Indicador Posición: PC5
+************************************************************************/
